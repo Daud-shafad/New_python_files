a28c5d9a97762ffde7a4741c9c2c1e9428fe918a
Daud-shafad <armyman10249@gmail.com>
2025-10-12 14:32:53 +0300

    last update was oct 12 at 2:32pm

---
+++ 29_days_roadmap/.history/day_6_20251008143134.py
@@ -0,0 +1,70 @@
+# Day 6: Simple library system (add/remove/search books).
+
+library_books_dict = {101: ["the big", "by: william smith"],
+                      102: ["the amazing", "by: john"],
+                      103: ["the money", "by: nicolas"],
+                      104: ["the president", "by: ahmed"]}
+
+books_borrowed = []
+person_borrowed = set()
+
+
+while True:
+    print("\n-----Available Option Menu-----")
+    print("1. View Available / present books")
+    print("2. Add book")
+    print("3. Remove book")
+    print("4. Search book by Key(ID)")
+    print("5. Borrow a book")
+    print("6. Exit")
+    
+    user_choice = input("Enter Your choice (1-5): ")
+    if user_choice == '6':
+        print("Good Bye, You Stopped the App!")
+        break
+    elif user_choice == '1' or user_choice == '2' or user_choice == '3' or user_choice == '4' or user_choice == '5':
+        
+        match user_choice:
+            
+            case '1':
+                print(f"The Available books are: {library_books_dict}")
+            
+            case '2':
+                user_id = int(input("Enter the ID: "))
+                user_book = input("Enter the book name: ")
+                user_author = input("Enter the author name: ")
+                if user_id not in  library_books_dict:
+                    library_books_dict[user_id] = [user_book, user_author]
+                    print(f"You Added a new book '{user_id}', for the name of '{user_book}', & author of '{user_author}'")
+                else:
+                    print("The book  is Already exist")
+            case '3':
+                user_id = int(input("Enter the ID: "))
+                if user_id in library_books_dict:
+                    library_books_dict.pop(user_id)
+                    print(f"You removed the book, that belongs the ID of {user_id}")
+                else:
+                    print("The ID you entered is not Exist, Add it firstly")
+            case '4':
+                user_id = int(input("Enter the ID: "))
+                if user_id in library_books_dict:
+                    library_books_dict.get(user_id)
+                    print(f"The book you searched is: {library_books_dict[user_id]}")
+                else:
+                    print("ID/Book  not Found")
+            case '5':
+                user_name = input ("Enter Your name: ")
+                
+                user_id = int(input("Enter the ID of the book you want to borrow: "))
+                if user_id in library_books_dict:
+                    print(f"You borrowed the book belongs the ID of {user_id}, please keep it.")
+                    books_borrowed.append(library_books_dict[user_id])
+                    person_borrowed.add(user_name)
+                    print(f"Borrowed books are: {books_borrowed}")
+                    print(f"the person borrowed is: {user_name} and he is in the collection of borrowers {person_borrowed}")
+                else:
+                    print("Book not found") 
+            case _:
+                print("Invalid option, try again")
+    else:
+        print("Invalid choice Type (1-5)")
